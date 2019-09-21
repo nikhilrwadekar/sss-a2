8232237074e1c8b85be5c770ccfef6615ad9e2cb
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-09-21 16:01:17 -0700

    Path, Middleware and a few new pages.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let app = express(); //Creates the Express Application
 // Path Library to handle any OS directory
 let path = require("path");
 
+// Using Middleware to handle static file paths
+// It serves it out to public access, unlike other file(s)/folder(s) which are inaccessible.
+app.use(express.static("public"));
+
 // Handling GET requests with Express
 
 // Root URL
@@ -13,6 +17,21 @@ app.get("/", (req, res) => {
 // /about URL - w/ Path
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/pages/about.html"));
+});
+
+// /contact /contact-us URL - w/ Path
+app.get(["/contact", "/contact-us"], (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/pages/contact.html"));
+});
+
+// /home.html redirecting to file - index.html - w/ CODE 301
+app.get("/home.html", (req, res) => {
+  res.redirect(301, "/index.html");
+});
+
+// If the routes above don't resolve anything.. 404 it is!
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "/public/pages/404.html"));
 });
 
 // Assign environment port or 8080
