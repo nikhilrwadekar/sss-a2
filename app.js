21f77d694b6f3470afd70c067bcb15c02ece43d3
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-09-24 17:38:20 -0700

    Final Changes

--- app.js
+++ app.js
@@ -14,14 +14,16 @@ app.get("/", (req, res) => {
   res.send("Hello, this is express!");
 });
 
+let routeToPages = [__dirname, "public/pages"];
+
 // /about URL - w/ Path
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/pages/about.html"));
+  res.sendFile(path.join(...routeToPages, "about.html"));
 });
 
 // /contact /contact-us URL - w/ Path
 app.get(["/contact", "/contact-us"], (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/pages/contact.html"));
+  res.sendFile(path.join(...routeToPages, "contact.html"));
 });
 
 // /home.html redirecting to file - index.html - w/ CODE 301
@@ -29,9 +31,46 @@ app.get("/home.html", (req, res) => {
   res.redirect(301, "/index.html");
 });
 
+// Coworkers and friends!
+let coworkers = [
+  "Angel",
+  "Jasmine",
+  "Blandy",
+  "Davinder",
+  "Satnam",
+  "Darshpreet"
+];
+let friends = {
+  Kiiroitori: {
+    type: "bird",
+    description: "hard worker"
+  },
+  Rilakkuma: {
+    type: "bear",
+    description: "likes to relax"
+  },
+  Korilakkuma: {
+    type: "bear",
+    description: "has a big imagination"
+  }
+};
+
+// Dynamic Routes
+let types = {
+  coworkers,
+  friends
+};
+
+app.get("/characters/:type", function(req, res, next) {
+  let type = req.params.type;
+  let matchedType = types[type];
+
+  matchedType ? res.send(matchedType) : next("route");
+});
+
 // If the routes above don't resolve anything.. 404 it is!
 app.get("*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "/public/pages/404.html"));
+  res.status(404).sendFile(path.join(...routeToPages, "404.html"));
 });
 
 // Assign environment port or 8080
